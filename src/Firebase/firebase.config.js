2a092cb5d7a99747b7c2eb8d5d7c8efbe24ba6b3
mdnajmul <mdnajmulovi@gmail.com>
2021-10-19 16:18:09 +0600

    firebase configeration set

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiN9mLq4tsnp_MgSZzUypbQY1qnZwVn54",
+  authDomain: "ema-john-simple-f2df6.firebaseapp.com",
+  projectId: "ema-john-simple-f2df6",
+  storageBucket: "ema-john-simple-f2df6.appspot.com",
+  messagingSenderId: "699749916745",
+  appId: "1:699749916745:web:7c67737282104ee8011f98",
+};
+
+export default firebaseConfig;
