234e90db2e35bc78db90e9a3df76ae994210db83
mdnajmul <mdnajmulovi@gmail.com>
2021-11-03 10:39:19 +0600

    fetch/get all products from server

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBiN9mLq4tsnp_MgSZzUypbQY1qnZwVn54",
-  authDomain: "ema-john-simple-f2df6.firebaseapp.com",
-  projectId: "ema-john-simple-f2df6",
-  storageBucket: "ema-john-simple-f2df6.appspot.com",
-  messagingSenderId: "699749916745",
-  appId: "1:699749916745:web:7c67737282104ee8011f98",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
